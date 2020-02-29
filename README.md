b4fd984f88c4a98bc880120855be035f4ff03f28
Jack Warren <jack@jackwarren.info>
2020-02-28 21:15:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Distributed CRL
+> ### Carter Codell, Edward Shen, Ivan Chen, Jack Warren
+>
+
+## Project Configuration
+IntelliJ should pick up the presence of Gradle within the project and ask to import it. After it is done, you'll want to use the `generateProto` Gradle task or use the "Regenerate Protobuf Code" IntelliJ run configuration I made.
+
+Everything here generally assumes you have Java 13, though any Kotlin bytecode is Java 8 compatible. If you're using an older version you might hit API issues if we use something that isn't available for you.
+
+Right now LibSodium is brought in as a dependency. You'll only need to have it installed if you try to run code that uses it (right now there's nothing that does).
+
+## Project Layout
+Each subproject contains a `src` folder that contains the project's code.
+
+Source code is in `src/main`, and test code is in `src/test`.
+
+Within each of those folders, code is organized by language. For example:
+- Kotlin source code would be in `src/main/kotlin`
+- Java test code would be in `src/test/java`
+- Protobuf files would be in `src/main/proto`
+
+All subprojects have `java` enabled. The **proto** subproject is the only one to have the `proto` source type enabled (don't worry, you can use the generated code from anywhere). All subprojects *other than* **proto** have the `kotlin` source type enabled (this helps prevent circular dependencies).
+
+
+### Applications
+Executable applications here include:
+- **certificate-authority**, our representation of a CA participant
+- **intermediary**, our representation of a non-CA participant
+- **client**, our representation of some non-participant client
+- **discovery-server**, a bootstrapping server to assist in this proof-of-concept
+
+Each application subproject has its entrypoint currently set as the `fun main()` in `src/main/kotlin/Main.kt`
+### Others
+Other libraries here include:
+- **shared**, a library of shared logic between the applications
+- **proto**, a store of Protobuf files for the entire project
---
+++ discovery-server/src/main/kotlin/Main.kt
@@ -0,0 +1,96 @@
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.int
+import io.ktor.network.selector.ActorSelectorManager
+import io.ktor.network.sockets.aSocket
+import io.ktor.network.sockets.openReadChannel
+import io.ktor.network.sockets.openWriteChannel
+import io.ktor.utils.io.jvm.javaio.toInputStream
+import io.ktor.utils.io.jvm.javaio.toOutputStream
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.net.InetSocketAddress
+import java.util.concurrent.ConcurrentHashMap
+
+/**
+ * This DiscoveryServer is an object, a Kotlin way of saying "a class that only ever has one instance so don't worry
+ * about constructors." It extends [CliktCommand], which gives us a few things:
+ * - Automatic help text
+ * - The ability to delegate properties to a command line option with the "by option" syntax
+ *      (This uses Kotlin's delegated properties, see https://kotlinlang.org/docs/reference/delegated-properties.html
+ *      if you're interested)
+ * - The ability to use [echo] (pretty much just [println] with some cross-platform compatibility thrown in)
+ */
+object DiscoveryServer : CliktCommand(help = "Run a discovery server to facilitate a proof-of-concept") {
+
+  private val port by option(help = "The port to run the discovery server on").int().default(22301)
+
+  private val serverSet: MutableSet<Discovery.Server> = ConcurrentHashMap.newKeySet()
+
+  /**
+   * This method is called by Clikt immediately upon all command line input being parsed. This is essentially the
+   * "main" method for this server.
+   */
+  override fun run() = runBlocking {
+    aSocket(ActorSelectorManager(Dispatchers.IO))
+      .tcp()
+      .bind(InetSocketAddress(port))
+      .use { serverSocket -> // `use` will always close the socket down correctly
+
+        echo("Discovery server online at ${serverSocket.localAddress} (from local observation)")
+
+        while (true) {
+          val socket = serverSocket.accept()
+
+          launch {
+            socket.use { socket ->
+              val input = socket.openReadChannel().toInputStream()
+              val output = socket.openWriteChannel(autoFlush = true).toOutputStream()
+              val remoteIP = (socket.remoteAddress as InetSocketAddress).hostString
+
+              // parseDelimitedFrom is like what we do for NSTP manually with writing an int of the size
+              // before every message, except it is more efficient and it is built-in to Java's protobuf
+              // code
+              val message = Discovery.FromClientMessage.parseDelimitedFrom(input)
+
+              when (message.messageCase) {
+                // Upon HELLO, respond with any existing servers and add the new one to the set
+                Discovery.FromClientMessage.MessageCase.HELLO -> {
+                  Discovery.Response.newBuilder().apply {
+                    addAllServers(serverSet)
+                  }.build().writeDelimitedTo(output)
+
+                  serverSet.add(makeServer(remoteIP, message.hello.port))
+                }
+                // Upon GOODBYE, remove it from the set
+                Discovery.FromClientMessage.MessageCase.GOODBYE ->
+                  serverSet.remove(makeServer(remoteIP, message.goodbye.port))
+                // If we can't parse, just print something
+                Discovery.FromClientMessage.MessageCase.MESSAGE_NOT_SET, null ->
+                  echo("Message from ${socket.remoteAddress} couldn't be parsed!")
+              }
+            }
+          }
+        }
+      }
+  }
+
+  /**
+   * Make a new Discovery.Server object for the given [hostName] and [port]
+   */
+  private fun makeServer(hostName: String, port: Int): Discovery.Server {
+    return Discovery.Server.newBuilder().apply {
+      this.hostName = hostName
+      this.port = port
+    }.build()
+  }
+
+}
+
+/**
+ * Essentially a dummy function, this redirects to the DiscoveryServer object that makes use of the Clikt
+ * command line parser
+ */
+fun main(args: Array<String>) = DiscoveryServer.main(args)
