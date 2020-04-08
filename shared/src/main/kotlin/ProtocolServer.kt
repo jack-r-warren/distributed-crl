e07c143c2a550925579ed7c12674343e953e3a61
Jack Warren <jack@jackwarren.info>
2020-04-07 22:50:17 -0400

    Da big push

---
+++ shared/src/main/kotlin/ProtocolServer.kt
@@ -0,0 +1,120 @@
+import com.google.protobuf.Message
+import io.ktor.network.sockets.isClosed
+
+abstract class ProtocolServer(val otherServers: MutableMap<NetworkIdentity, SocketTuple>) {
+
+  /*
+  Socket stuff
+   */
+
+  // Helper function to send some message to some identity
+  fun sendMessageToIdentity(identity: NetworkIdentity, message: Dcrl.DCRLMessage): Unit {
+    otherServers[identity]?.let { socket ->
+      message.writeTo(socket.outputStream)
+    } ?: println("Was asked to send a message to $identity but it didn't exist in the otherServers map!")
+  }
+
+  // This function is called by [runProtocolServer] to have a coroutine sit around and babysit a socket
+  fun babysitSocket(identity: NetworkIdentity, socket: SocketTuple) {
+    try {
+      // Receive messages and send any non-null outputs of the handleMessage function
+      while (true)
+        handleMessage(identity, Dcrl.DCRLMessage.parseDelimitedFrom(socket.inputStream))?.writeTo(socket.outputStream)
+    } catch (e: Throwable) {
+      println("Error bubbled up to socket handling, so the socket ($identity) was closed.")
+      println(e)
+    } finally {
+      kotlin.runCatching { if (!socket.isClosed) socket.close() }
+      otherServers.remove(identity)
+    }
+  }
+
+  /*
+  Message handling stuff
+   */
+
+  private fun failOnNotSet(message: Message): Nothing =
+    throw IllegalArgumentException("Message case in $message was not set")
+
+  private fun failOnNull(message: Message): Nothing =
+    throw IllegalArgumentException("Message case in $message wasn't recognized (did you add a new case?)")
+
+  fun handleMessage(identity: NetworkIdentity, message: Dcrl.DCRLMessage): Dcrl.DCRLMessage? =
+    when (message.messageCase) {
+      Dcrl.DCRLMessage.MessageCase.UNSIGNED_MESSAGE -> handleMessage(identity, message.unsignedMessage)
+      Dcrl.DCRLMessage.MessageCase.SIGNED_MESSAGE -> handleMessage(identity, message.signedMessage)
+      Dcrl.DCRLMessage.MessageCase.MESSAGE_NOT_SET -> failOnNotSet(message)
+      null -> failOnNull(message)
+    }
+
+  fun handleMessage(identity: NetworkIdentity, message: Dcrl.UnsignedMessage): Dcrl.DCRLMessage? =
+    when (message.messageCase) {
+      Dcrl.UnsignedMessage.MessageCase.BLOCKCHAIN_REQUEST -> handleMessage(identity, message.blockchainRequest)
+      Dcrl.UnsignedMessage.MessageCase.BLOCK_REQUEST -> handleMessage(identity, message.blockRequest)
+      Dcrl.UnsignedMessage.MessageCase.ERROR_MESSAGE -> handleMessage(identity, message.errorMessage)
+      Dcrl.UnsignedMessage.MessageCase.MESSAGE_NOT_SET -> failOnNotSet(message)
+      else -> failOnNull(message)
+    }
+
+  abstract fun handleMessage(identity: NetworkIdentity, message: Dcrl.BlockchainRequest): Dcrl.DCRLMessage?
+  abstract fun handleMessage(identity: NetworkIdentity, message: Dcrl.BlockRequest): Dcrl.DCRLMessage?
+  abstract fun handleMessage(
+    identity: NetworkIdentity,
+    message: Dcrl.ErrorMessage,
+    from: Dcrl.Certificate? = null
+  ): Dcrl.DCRLMessage?
+
+  fun handleMessage(identity: NetworkIdentity, message: Dcrl.SignedMessage): Dcrl.DCRLMessage? =
+    when (message.messageCase) {
+      Dcrl.SignedMessage.MessageCase.CERTIFICATE_REVOCATION -> handleMessage(
+        identity,
+        message.certificateRevocation,
+        message.certificate
+      )
+      Dcrl.SignedMessage.MessageCase.BLOCK_MESSAGE -> handleMessage(identity, message.blockMessage, message.certificate)
+      Dcrl.SignedMessage.MessageCase.BLOCKCHAIN_RESPONSE -> handleMessage(
+        identity,
+        message.blockchainResponse,
+        message.certificate
+      )
+      Dcrl.SignedMessage.MessageCase.BLOCK_RESPONSE -> handleMessage(
+        identity,
+        message.blockResponse,
+        message.certificate
+      )
+      Dcrl.SignedMessage.MessageCase.ERROR_MESSAGE -> handleMessage(identity, message.errorMessage, message.certificate)
+      Dcrl.SignedMessage.MessageCase.ANNOUNCE -> handleMessage(identity, message.announce, message.certificate)
+      Dcrl.SignedMessage.MessageCase.MESSAGE_NOT_SET -> failOnNotSet(message)
+      else -> failOnNull(message)
+    }
+
+  abstract fun handleMessage(
+    identity: NetworkIdentity,
+    message: Dcrl.CertificateRevocation,
+    from: Dcrl.Certificate
+  ): Dcrl.DCRLMessage?
+
+  abstract fun handleMessage(
+    identity: NetworkIdentity,
+    message: Dcrl.BlockMessage,
+    from: Dcrl.Certificate
+  ): Dcrl.DCRLMessage?
+
+  abstract fun handleMessage(
+    identity: NetworkIdentity,
+    message: Dcrl.BlockchainResponse,
+    from: Dcrl.Certificate
+  ): Dcrl.DCRLMessage?
+
+  abstract fun handleMessage(
+    identity: NetworkIdentity,
+    message: Dcrl.BlockResponse,
+    from: Dcrl.Certificate
+  ): Dcrl.DCRLMessage?
+
+  abstract fun handleMessage(
+    identity: NetworkIdentity,
+    message: Dcrl.Announce,
+    from: Dcrl.Certificate
+  ): Dcrl.DCRLMessage?
+}
---
+++ shared/src/main/kotlin/ProtocolServerUtil.kt
@@ -0,0 +1,84 @@
+import io.ktor.network.selector.ActorSelectorManager
+import io.ktor.network.sockets.aSocket
+import io.ktor.network.sockets.isClosed
+import io.ktor.network.sockets.openReadChannel
+import io.ktor.network.sockets.openWriteChannel
+import io.ktor.utils.io.jvm.javaio.toInputStream
+import io.ktor.utils.io.jvm.javaio.toOutputStream
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.net.InetSocketAddress
+
+// An plain function that will run a given protocol server. Command line needs to be parsed or whatever before
+// calling this function
+fun runProtocolServer(
+  discoveryServer: NetworkIdentity,
+  becomeDiscoverable: Boolean = true,
+  protocolServerFactory: (MutableMap<NetworkIdentity, SocketTuple>) -> ProtocolServer
+): Unit {
+  runBlocking {
+    // Make the server socket
+    val serverSocket = aSocket(ActorSelectorManager(Dispatchers.IO)).tcp().bind()
+
+    // Compute the server's port as late as possible (but keep track of it in case something happens
+    val serverSocketPort by lazy { (serverSocket.localAddress as InetSocketAddress).port }
+
+    // Inform the discovery server of us and initiate a client connection to everyone the
+    // discovery server is keeping track of
+    val otherServers = aSocket(ActorSelectorManager(Dispatchers.IO))
+      .tcp().connect(discoveryServer).use { discoverySocket ->
+        // Tell the server about us (if we should)
+        when (becomeDiscoverable) {
+          true -> Discovery.Hello.newBuilder().apply { this.port = serverSocketPort }.build()
+          false -> Discovery.Hello.getDefaultInstance()
+        }.writeTo(discoverySocket.openWriteChannel(true).toOutputStream())
+
+        // Parse the server's response
+        Discovery.Response.parseDelimitedFrom(discoverySocket.openReadChannel().toInputStream()).serversList.map {
+          NetworkIdentity(it.ipAddress, it.portNumber)
+        }.map {
+          Pair<NetworkIdentity, SocketTuple>(
+            it, SocketTuple(aSocket(ActorSelectorManager(Dispatchers.IO)).tcp().connect(it))
+          )
+        }.toMap(mutableMapOf())
+      }
+
+    try {
+      protocolServerFactory(otherServers).let { protocolServer: ProtocolServer ->
+        // For every server we know about, initiate a socket
+        for ((identity, socket) in protocolServer.otherServers)
+          launch { protocolServer.babysitSocket(identity, socket) }
+
+        // Listen for any future connections, accept each as a socket
+        while (true) SocketTuple(serverSocket.accept()).let { socket ->
+          NetworkIdentity.from(socket).let { identity ->
+            protocolServer.otherServers[identity] = socket
+            launch { protocolServer.babysitSocket(identity, socket) }
+          }
+        }
+      }
+    } finally {
+
+      // Clean up all the sockets
+      for (socket in otherServers.values)
+        kotlin.runCatching { if (!socket.isClosed) socket.close() }
+
+      if (becomeDiscoverable) {
+        // Tell the discovery server that we are shutting down
+        aSocket(ActorSelectorManager(Dispatchers.IO))
+          .tcp().connect(discoveryServer).use { discoverySocket ->
+            Discovery.Goodbye.newBuilder().apply {
+              this.port = serverSocketPort
+            }.build().writeTo(discoverySocket.openWriteChannel(true).toOutputStream())
+          }
+      }
+
+      // Try to close the server socket and swallow any related errors
+      kotlin.runCatching { if (!serverSocket.isClosed) serverSocket.close() }
+    }
+
+
+  }
+}
+
