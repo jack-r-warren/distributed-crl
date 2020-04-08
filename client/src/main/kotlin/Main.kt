9c2c52938e880e7b46bb8e9b2eeadd7e88ab338d
Jack Warren <jack@jackwarren.info>
2020-04-08 06:32:44 -0400

    Add a little Netty server

--- client/src/main/kotlin/Main.kt
+++ client/src/main/kotlin/Main.kt
@@ -1,1 +1,42 @@
-fun main(args: Array<String>) : Nothing = TODO()
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.convert
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.types.int
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) = ClientMain.main(args)
+
+object ClientMain : CliktCommand() {
+  private val discovery: NetworkIdentity by option().convert { NetworkIdentity.from(it) }.required()
+  private val webPort: Int by option().int().required()
+
+  override fun run() = runProtocolServer(
+    discoveryServer = discovery,
+    // Method reference syntax, used here to reference a constructor...
+    protocolServerFactory = ::ObserverRoleServer,
+    // ...and used here to reference a method of this class
+    callbackWithConfiguredServer = ::runWebInterface
+  )
+
+  private fun runWebInterface(server: ObserverRoleServer): Unit {
+    embeddedServer(Netty, webPort) {
+      routing {
+        get("/") {
+          call.respondText("Go to /check/{hash} to check the validity of a certificate", ContentType.Text.Html)
+        }
+        get("/check/{hash}") {
+          call.parameters["hash"]?.let {
+            call.respondText(ContentType.Text.Html) { server.checkCertificate(it).name }
+          } ?: call.respondText("No certificate hash given as a parameter", ContentType.Text.Html)
+        }
+      }
+    }.start(wait = true)
+  }
+}
