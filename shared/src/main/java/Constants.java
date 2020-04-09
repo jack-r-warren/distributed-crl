1fbd71e90b763612d2129a9a782864e7f88a8166
Carter Codell <codell.c@husky.neu.edu>
2020-04-09 17:28:48 -0400

    More Constants

--- shared/src/main/java/Constants.java
+++ shared/src/main/java/Constants.java
@@ -5,13 +5,20 @@ import java.util.ArrayList;
 
 public class Constants {
 
+  public static final Dcrl.Certificate GENESIS_BLOCK_CERTIFICATE = Dcrl.Certificate.getDefaultInstance();
+  public static final int GENESIS_BLOCK_HEIGHT = 0;
+  public static final byte[] GENESIS_BLOCK_PREV = new byte[32];
+  public static final long GENESIS_BLOCK_TIMESTAMP = 1586466355631L;
+  public static final byte[] GENESIS_BLOCK_MERKLE_ROOT = Util.merkleRoot(new ArrayList<Dcrl.CertificateRevocation>());
+
   public static final Dcrl.BlockMessage GENESIS_BLOCK = Dcrl.BlockMessage.newBuilder()
-      .setCertificate(Dcrl.Certificate.getDefaultInstance())
-      .setHeight(0)
-      .setPreviousBlock(ByteString.copyFrom(new byte[32])) // array of all 0s same size as sha256 hash
-      .setTimestamp(1586466355631L)
-      .setMerkleRoot(ByteString.copyFrom(Util.merkleRoot(new ArrayList<Dcrl.CertificateRevocation>())))
+      .setCertificate(GENESIS_BLOCK_CERTIFICATE)
+      .setHeight(GENESIS_BLOCK_HEIGHT)
+      .setPreviousBlock(ByteString.copyFrom(GENESIS_BLOCK_PREV)) // array of all 0s same size as sha256 hash
+      .setTimestamp(GENESIS_BLOCK_TIMESTAMP)
+      .setMerkleRoot(ByteString.copyFrom(GENESIS_BLOCK_MERKLE_ROOT))
       .build();
 
   public static final byte[] GENESIS_BLOCK_HASH = Util.hash(GENESIS_BLOCK);
+
 }
