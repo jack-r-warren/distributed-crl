0d9534cc71e6384dcb38006bb716b3ba5957553a
Carter Codell <codell.c@husky.neu.edu>
2020-04-09 10:11:46 -0400

    Changed the signature for the verifying function.

--- shared/src/main/java/SigningUtil.java
+++ shared/src/main/java/SigningUtil.java
@@ -18,10 +18,54 @@ public class SigningUtil {
     return CryptoKt.sign(message.toByteArray(), private_key);
   }
 
-  // TODO see my comments on Ivan's PR.
-  public static boolean verify(GeneratedMessageV3 message,
-                                       byte[] signature,
-                                       byte[] public_key) {
+  public static boolean verify(Dcrl.SignedMessage signedMessage) {
+    if (signedMessage == null) {
+      return false;
+    }
+
+    // get inner message from signedMessage
+    GeneratedMessageV3 message;
+    switch (signedMessage.getMessageCase()) {
+      case CERTIFICATE_REVOCATION:
+        message = signedMessage.getCertificateRevocation();
+        break;
+      case BLOCK_MESSAGE:
+        message = signedMessage.getBlockMessage();
+        break;
+      case BLOCKCHAIN_RESPONSE:
+        message = signedMessage.getBlockchainResponse();
+        break;
+      case BLOCK_RESPONSE:
+        message = signedMessage.getBlockResponse();
+        break;
+      case ERROR_MESSAGE:
+        message = signedMessage.getErrorMessage();
+        break;
+      case ANNOUNCE:
+        message = signedMessage.getAnnounce();
+        break;
+      default: // handles MESSAGE_NOT_SET case
+        return false;
+    }
+
+    // get signature from signedMessage
+    byte[] signature = signedMessage.getSignature().toByteArray();
+    // error checking
+    if (signature.length == 0) {
+      return false;
+    }
+
+    // get public key from cert
+    Dcrl.Certificate cert = signedMessage.getCertificate();
+    // error checking
+    if (cert.equals(Dcrl.Certificate.getDefaultInstance())) {
+      return false;
+    }
+    byte[] public_key = cert.getSigningPublicKey().toByteArray();
+    if (public_key.length == 0) {
+      return false;
+    }
+
     return CryptoKt.verifySign(message.toByteArray(), signature, public_key);
   }
 
