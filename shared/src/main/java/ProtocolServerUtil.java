45fac06824ee8e312637b6e7da346693cd2f899a
Carter Codell <codell.c@husky.neu.edu>
2020-04-09 13:57:01 -0400

    Forgot we need a private key.
    private key and signature cannot be null.

--- shared/src/main/java/ProtocolServerUtil.java
+++ shared/src/main/java/ProtocolServerUtil.java
@@ -1,25 +1,39 @@
-import javax.annotation.Nullable;
+import com.google.protobuf.ByteString;
+import org.jetbrains.annotations.NotNull;
 
 public class ProtocolServerUtil {
-  public static Dcrl.DCRLMessage buildErrorMessage(String error, @Nullable Dcrl.Certificate certificate) {
-    if (certificate == null) {
-      Dcrl.DCRLMessage message = Dcrl.DCRLMessage.newBuilder()
+  public static Dcrl.DCRLMessage buildErrorMessage(String errorString,
+                                                   @NotNull Dcrl.Certificate certificate,
+                                                   @NotNull byte[] private_key) {
+
+    Dcrl.DCRLMessage message;
+    if (certificate.equals(Dcrl.Certificate.getDefaultInstance()) || private_key.length == 0) {
+      message = Dcrl.DCRLMessage.newBuilder()
           .setUnsignedMessage(
               Dcrl.UnsignedMessage.newBuilder()
                   .setErrorMessage(
                       Dcrl.ErrorMessage.newBuilder()
-                          .setError(error)
+                          .setError(errorString)
                   )
           )
           .build();
       return message;
     } else {
-      // TODO create and sign the error message.
-      return null;
+      Dcrl.ErrorMessage error = Dcrl.ErrorMessage.newBuilder().setError(errorString).build();
+
+      message = Dcrl.DCRLMessage.newBuilder()
+          .setSignedMessage(
+              Dcrl.SignedMessage.newBuilder()
+                .setCertificate(certificate)
+                .setSignature(ByteString.copyFrom(new byte[0])) // TODO TODO TODO need to change this so it actually signs the message!!!!
+                .setErrorMessage(error)
+          )
+          .build();
     }
+    return message;
   }
 
   public static Dcrl.DCRLMessage buildErrorMessage(String error) {
-    return buildErrorMessage(error, null);
+    return buildErrorMessage(error, Dcrl.Certificate.getDefaultInstance(), new byte[0]);
   }
 }
